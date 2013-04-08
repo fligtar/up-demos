c0b861e8a6e37af1af14c5a645157dd858f968ab
Justin Scott <fligtar@gmail.com>
2013-04-08 16:57:55 -0700

    switch to tumblr for tags

--- demo.js
+++ demo.js
@@ -45,7 +45,8 @@ function populateInterests(_int) {
         var lead = "Showing photos relating to your top interests of ";
         $.each(_int, function(k, v) {
             if (j < 5) {
-                getPhotosByTag(v['name']);
+                $("#content").append('<div id="interest' + (j + 1) + '"><h3>' + (j + 1) + '. ' + v['name'] + '</h3><ul class="thumbnails"></ul></div><hr>');
+                getPostsByTag(v['name'], j + 1);
                 
                 if (j != 0) {
                     lead += ", ";
@@ -69,17 +70,41 @@ function populateInterests(_int) {
 function getRecentPhotos() { $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.getrecent&api_key=e4d4d87b4fa8b0f25a6ff2fa6ed86f6c&per_page=50&format=json&nojsoncallback=1", addPhotosToWall);
 }
 
-function getPhotosByTag(tag) {   $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e4d4d87b4fa8b0f25a6ff2fa6ed86f6c&tags=" + tag + "&safe_search=1&media=photos&per_page=20&format=json&nojsoncallback=1", addPhotosToWall);
+function addPhotosToWall(data) {
+    $("#content").append('<ul class="thumbnails"></ul>');
+    var ul = $('#content .thumbnails');
+    
+    $.each(data['photos']['photo'], function(k, v) {
+        ul.append('<li class="span3"><div class="thumbnail"><a href="http://www.flickr.com/photos/' + v['owner'] + '/' + v['id'] + '"><img src="http://farm' + v['farm'] + '.staticflickr.com/' + v['server'] + '/' + v['id'] + '_' + v['secret'] + '_m.jpg"/>' + (v['title'] != '' ? '<p><small>' + v['title'].trunc(70, true) + '</small></p>' : '') + '</a></div></li>');
+    });
+    ul.imagesLoaded( function() { ul.masonry(); });
 }
 
-function addPhotosToWall(data) {
-  $.each(data['photos']['photo'], function(k, v) {
-    $("#photos").append("<div class=\"span2\"><a href=\"http://www.flickr.com/photos/" + v['owner'] + "/" + v['id'] + "\"><img src=\"http://farm" + v['farm'] + ".staticflickr.com/" + v['server'] + "/" + v['id'] + "_" + v['secret'] + "_m.jpg\"/></a></div>");
-  });
+function getPostsByTag(tag, int_num) {
+    $('body').append('<script src="http://api.tumblr.com/v2/tagged?tag=' + tag + '&filter=text&&api_key=AOiwk96Nu7FpdEMEFkDAal0A8bhvsFJquJh2XNLh1RcNOl6shp&callback=jsonpInterest(' + int_num + ')"></script>');
 }
+
+function addPosts(data, int_num) {
+    var ul = $("#interest" + int_num + " ul");
+    $.each(data.response, function(k, v) {
+        if (v['type'] == 'photo') {
+          ul.append('<li class="span3"><div class="thumbnail"><a href="' + v['post_url'] + '" target="_blank"><img src="' + v['photos'][0]['alt_sizes'][1]['url'] + '"/>' + (v['caption'] != '' ? '<p><small>' + v['caption'].trunc(70, true) + '</small></p>' : '') + '</a></div></li>');
+        }
+    });
+    ul.imagesLoaded( function() { ul.masonry(); });
+}
+
+function jsonpInterest(num) { return function(data) { addPosts(data, num); } }
 
 function clearWall() {
-  $("#photos").html("");
+  $("#content").html("");
 }
 
+String.prototype.trunc =
+     function(n,useWordBoundary){
+         var toLong = this.length>n,
+             s_ = toLong ? this.substr(0,n-1) : this;
+         s_ = useWordBoundary && toLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
+         return  toLong ? s_ + '&hellip;' : s_;
+      };
 $(document).ready(getRecentPhotos);
